0b083c6873da3650f5b22fa4f80d28296bd84d7a
Sohan817 <hsohan509@gmail.com>
2023-09-10 23:26:52 +0600

    creating thunderClient colletion to manage request and refactoring auth.js

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -3,7 +3,7 @@ const mongoURI = "mongodb://127.0.0.1:27017/INotebook";
 
 const connectToMongo = async () => {
   mongoose.connect(mongoURI);
-  console.log("Connected to Mongo Successfully!");
+  console.log("Connected to MongoDB Successfully!");
 };
 
 module.exports = connectToMongo;
--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -3,29 +3,36 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
-//Create a user usigg:POST "/api/auth" . Dosen't require auth
+//Create a user usigg:POST "/api/auth/createuser" . Dosen't require auth
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
     body("password", "Pasword must be 5 characters").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //If there are errors return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ err: "Enter a unique value for email" });
+    //check whether the user with this email exits already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ errors: "Sorry! Email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 module.exports = router;
