f1f95771ec6d2e6076e7cf011c5a79b6e131ab4d
Sohan817 <hsohan509@gmail.com>
2023-09-10 17:27:53 +0600

    Storing data into the database using mongoose model

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -4,10 +4,10 @@ connectToMongo();
 
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 //Available Routes
-app.use("/app/auth", require("./routes/auth"));
-app.use("/app/notes", require("./routes/notes"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.json([]);
+//Create a user usigg:POST "/api/auth" . Dosen't require auth
+router.post("/", (req, res) => {
+  res.send(req.body);
+  // @ts-ignore
+  const user = User(req.body);
+  user.save();
+  console.log(req.body);
 });
 module.exports = router;
